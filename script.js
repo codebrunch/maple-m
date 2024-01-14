ebc6d6a3cbe082f01f31b894efa378b67a915fed
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-14 19:49:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const characterNameInput = document.getElementById('characterName');
+
+    // 포커스 이벤트 처리
+    characterNameInput.addEventListener('focus', function () {
+        if (characterNameInput.value === '수행자명을 입력해주세요.') {
+            characterNameInput.value = '';
+        }
+    });
+
+    // 포커스 아웃 이벤트 처리
+    characterNameInput.addEventListener('blur', function () {
+        if (characterNameInput.value === '') {
+            characterNameInput.value = '수행자명을 입력해주세요.';
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
     const characterNameInput = document.getElementById('characterName');
     const serverSelect = document.getElementById('serverSelect'); // 추가된 부분 
 
@@ -14,40 +33,40 @@ document.addEventListener('DOMContentLoaded', function () {
         if (characterNameInput.value === '') {
             characterNameInput.value = '수행자명을 입력해주세요.';
         }
-    }); 
+    });
+});
 
-    async function fetchCharacterInfo() {
-        const apiKey = 'test_7bd80f4de26d58e5774b999a4fa19bfeedcbecdcd4e10ae62c93010860e37a6ba15bcd2f39f06a91bca84af63852f7c1';
-        const characterName = characterNameInput.value;
-        const server = serverSelect.value; 
+async function fetchCharacterInfo() {
+      const apiKey = 'test_7bd80f4de26d58e5774b999a4fa19bfeedcbecdcd4e10ae62c93010860e37a6ba15bcd2f39f06a91bca84af63852f7c1';
+      const characterName = characterNameInput.value;
+      const server = serverSelect.value; 
 
-        if (!characterName || !server) {
-            alert("수행자명과 서버를 입력 후 검색해주세요.");
-            return;
-        } 
+      if (!characterName || !server) {
+          alert("수행자명과 서버를 입력 후 검색해주세요.");
+          return;
+      } 
 
-        const idApiUrl = `https://open.api.nexon.com/maplestorym/v1/character/search?character_name=${characterName}&server=${server}&apiKey=${apiKey}`; 
+      const idApiUrl = `https://open.api.nexon.com/maplestorym/v1/character/search?character_name=${characterName}&server=${server}&apiKey=${apiKey}`; 
 
-        fetch(idApiUrl)
-            .then(response => response.json())
-            .then(idData => {
-                updateCharacterInfo(idData); 
+      fetch(idApiUrl)
+          .then(response => response.json())
+          .then(idData => {
+              updateCharacterInfo(idData); 
 
-                const equipmentApiUrl = `https://open.api.nexon.com/maplestorym/v1/character/item-equipment?ocid=${idData.ocid}&apiKey=${apiKey}`;
-                return fetch(equipmentApiUrl);
-            })
-            .then(response => response.json())
-            .then(equipmentData => {
-                updateEquipmentInfo(equipmentData);
-            })
-            .catch(error => {
-                console.error('정보를 가져오는 도중 에러 발생:', error);
-            });
-    } 
+              const equipmentApiUrl = `https://open.api.nexon.com/maplestorym/v1/character/item-equipment?ocid=${idData.ocid}&apiKey=${apiKey}`;
+              return fetch(equipmentApiUrl);
+          })
+          .then(response => response.json())
+          .then(equipmentData => {
+              updateEquipmentInfo(equipmentData);
+          })
+          .catch(error => {
+              console.error('정보를 가져오는 도중 에러 발생:', error);
+          });
+  const fetchButton = document.querySelector('button');
+  fetchButton.addEventListener('click', fetchCharacterInfo); 
+} 
 
-    const fetchButton = document.querySelector('button');
-    fetchButton.addEventListener('click', fetchCharacterInfo);
-}); 
 
 function updateCharacterInfo(data) {
     const characterInfoContainer = document.getElementById('characterResult'); 
