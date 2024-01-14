ca2a09532ee732a91bfa2b388b75549cd7962d9c
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-14 19:51:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const characterNameInput = document.getElementById('characterName');
-
-    // 포커스 이벤트 처리
-    characterNameInput.addEventListener('focus', function () {
-        if (characterNameInput.value === '수행자명을 입력해주세요.') {
-            characterNameInput.value = '';
-        }
-    });
-
-    // 포커스 아웃 이벤트 처리
-    characterNameInput.addEventListener('blur', function () {
-        if (characterNameInput.value === '') {
-            characterNameInput.value = '수행자명을 입력해주세요.';
-        }
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
     const characterNameInput = document.getElementById('characterName');
     const serverSelect = document.getElementById('serverSelect'); // 추가된 부분 
 
