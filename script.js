515fa437ce40fc750ce60b8908b8c53b22c67b4d
codebrunch <70242287+codebrunch@users.noreply.github.com>
2024-01-16 21:43:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // 포커스 이벤트 처리
     characterNameInput.addEventListener('focus', function () {
-        if (characterNameInput.value === '수행자명을 입력해주세요.') {
+        if (characterNameInput.value === '캐릭터명을 입력해주세요.') {
             characterNameInput.value = '';
         }
     });
@@ -12,45 +12,50 @@ document.addEventListener('DOMContentLoaded', function () {
     // 포커스 아웃 이벤트 처리
     characterNameInput.addEventListener('blur', function () {
         if (characterNameInput.value === '') {
-            characterNameInput.value = '수행자명을 입력해주세요.';
+            characterNameInput.value = '캐릭터명을 입력해주세요.';
         }
     });
 
+    const fetchButton = document.querySelector('button');
+    fetchButton.addEventListener('click', fetchCharacterInfo); // 버튼 클릭 시 fetchCharacterInfo 함수 호출
+
     async function fetchCharacterInfo() {
         const apiKey = 'test_7bd80f4de26d58e5774b999a4fa19bfeedcbecdcd4e10ae62c93010860e37a6ba15bcd2f39f06a91bca84af63852f7c1';
-        const characterName = characterNameInput.value;
+        const characterName = encodeURIComponent(characterNameInput.value.trim());
         const server = serverSelect.value;
-    
+
         if (!characterName || !server) {
-            alert("수행자명과 서버를 입력 후 검색해주세요.");
+            alert("캐릭터명과 서버를 입력 후 검색해주세요.");
             return;
         }
-    
-        const searchApiUrl = `https://open.api.nexon.com/maplestorym/v1/character/search?character_name=${characterName}&server=${server}&apiKey=${apiKey}`;
-    
+
+        const searchApiUrl = `https://open.api.nexon.com/maplestorym/v1/id?character_name=${characterName}&world_name=${server}`;
+
         try {
-            const searchResponse = await fetch(searchApiUrl);
-            const idData = await searchResponse.json();
-    
-            if (!idData.ocid) {
-                throw new Error('ocid not found in the response');
-            }
-    
+            const response = await fetch(url, {
+                headers: {
+                    "x-nxopen-api-key": baramyconfig,
+                },
+            });
+
+            const idData = await response.json();
+
+            //if (!idData.ocid) {
+            //    throw new Error('ocid not found in the response');
+            //}
+
             const ocid = idData.ocid;
-    
+            console.log(ocid);
+            /*
             const idApiUrl = `https://open.api.nexon.com/maplestorym/v1/id?ocid=${ocid}&apiKey=${apiKey}`;
-    
+
             const idResponse = await fetch(idApiUrl);
             const characterInfo = await idResponse.json();
-    
+            */
             // 이제 characterInfo 변수에 원하는 정보가 들어있습니다.
-            console.log(characterInfo);
+            //console.log(characterInfo);
         } catch (error) {
             console.error('정보를 가져오는 도중 에러 발생:', error);
         }
     }
-
-
-    const fetchButton = document.querySelector('button');
-    fetchButton.addEventListener('click', fetchCharacterInfo);
 });
